fa43bca926c74bf1f8324c9ebc9d39590c25c1ae
evanxia1018 <evanxia1018@gmail.com>
2017-04-29 12:26:33 -0700

    Fix bugs where printf porbits all format string characters

--- Evan/safe_lib.c
+++ Evan/safe_lib.c
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <dlfcn.h>
 #include <stdlib.h>
+#include <string.h>
 #include <stdarg.h>
 
 typeof(printf) *old_printf;
@@ -11,26 +12,37 @@ int printf(const char *format,...)
 {
 	// get a pointer to the function "printf"
         old_printf = dlsym(RTLD_NEXT, "printf");
-	(*old_printf)("You are now doing safe_printf: ");// for demo and debug purpose
+	//(*old_printf)("You are now doing safe_printf: ");// for demo and debug purpose
+	va_list list;
+        char *parg;
         /*
                 SANITIZE INPUT
         */
 	
-	if((checkinput(format,'%n') +
-	checkinput(format,'%hhn') +
-	checkinput(format,'%hn') +
-	checkinput(format,'%ln') +
-	checkinput(format,'%lln') +
-	checkinput(format,'%Ln') +
-	checkinput(format,'%zn') +
-	checkinput(format,'%jn') +
-	checkinput(format,'%tn')) 
-	== 0) (*old_printf)("%s",format); // and we call the function with previous arguments
+	if((checkinput(format,"%n")+
+		checkinput(format,"%hhn") +
+		checkinput(format,"%hn") +
+		checkinput(format,"%ln") +
+		checkinput(format,"%lln") +
+		checkinput(format,"%Ln") +
+		checkinput(format,"%zn") +
+		checkinput(format,"%jn") +
+		checkinput(format,"%tn")) == 0){
+		// format variable arguments
+        	va_start(list, format);
+        	vasprintf(&parg, format, list);
+        	va_end(list);
+		(*old_printf)("%s",parg); // and we call the function with previous arguments
+		free(parg);
+	} 
 	else (*old_printf)("ERROR: User input contains prohibited characters.\n");
-	
 }
 
-int checkinput(char* input,int forbidden){
-	if(strchr(input,forbidden))	return 1;
+int checkinput(char* input,char* forbidden){
+	if(strstr(input,forbidden)!=NULL)	return 1;
 	return 0;
 }
+
+
+
+
