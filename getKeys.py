ecccbfa3dc4df9db30fc716e3a1c036196a7998f
evanxia1018 <evanxia1018@gmail.com>
2017-05-01 15:39:07 -0700

    move tools to working directory

---
+++ getKeys.py
@@ -0,0 +1,19 @@
+#!/bin/env python
+
+from ictf import iCTF
+
+i = iCTF("http://35.167.152.77/")
+t = i.login("evanxia1018@gmail.com","B3Uy3rYgqza8")
+
+key_info = t.get_ssh_keys()
+
+with open("ctf_key", 'wb') as f:
+    f.write(key_info['ctf_key'])
+
+with open("root_key", 'wb') as f:
+    f.write(key_info['root_key'])
+
+print "IP: ", key_info['ip']
+print "PORT: ", key_info['port']
+
+# targets = t.get_targets(<service ID>)
---
+++ login_root.sh
@@ -0,0 +1,1 @@
+ssh -i root_key -p 1356 root@52.89.52.99
